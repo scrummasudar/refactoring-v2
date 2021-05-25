3a665cbf1e6662bde50005e0a9f4fa384b1caf79
scrummasudar <kentaro.masuda227@gmail.com>
2021-05-25 23:05:40 +0900

    Page13-パラメータを削除

--- chapter1.js
+++ chapter1.js
@@ -2,7 +2,7 @@ function statement (invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
     case "tragedy":
@@ -32,7 +32,7 @@ function statement (invoice, plays) {
                           minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
