942b4bba347e3ed065a2897a18313dfb3dbfa607
scrummasudar <kentaro.masuda227@gmail.com>
2021-05-25 22:53:18 +0900

    Page11-問い合わせによる一時変数の置き換え

--- chapter1.js
+++ chapter1.js
@@ -1,4 +1,7 @@
 function statement (invoice, plays) {
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
   function amountFor(aPerformance, play) {
     let result = 0;
     switch (play.type) {
@@ -29,7 +32,7 @@ function statement (invoice, plays) {
                           minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // ボリューム特典のポイントを加算
