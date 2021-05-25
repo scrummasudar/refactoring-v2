1cacf8ffd68a358a12e38681ba1f7be9f5edfa94
scrummasudar <kentaro.masuda227@gmail.com>
2021-05-25 22:38:04 +0900

    Page7-switch文を関数の抽出

--- chapter1.js
+++ chapter1.js
@@ -1,15 +1,6 @@
 function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",
-                        { style: "currency", currency: "USD",
-                          minimumFractionDigits: 2 }).format;
-
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+  function amountFor(perf, play) {
     let thisAmount = 0;
-
     switch (play.type) {
     case "tragedy":
       thisAmount = 40000;
@@ -25,8 +16,21 @@ function statement (invoice, plays) {
       thisAmount += 300 * perf.audience;
       break;
     default:
-      throw new Error(`unkown type: ${play.type}`)
+      throw new Error(`unkown type: ${play.type}`);
     }
+    return thisAmount;
+  }
+
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US",
+                        { style: "currency", currency: "USD",
+                          minimumFractionDigits: 2 }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
 
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
